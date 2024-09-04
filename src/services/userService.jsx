03c29b1713e03d30cd11e8e124c46d8139c9a1d3
SariCohen2 <s0556774746r@gmail.com>
2024-09-04 23:50:14 +0300

    404 error handling

--- src/services/userService.jsx
+++ src/services/userService.jsx
@@ -1,6 +1,6 @@
 // services/userService.js
 // const API_URL = 'http://10.217.59.99:5000';
-const API_URL='https://KobiStam.onrender.com';
+const API_URL='https://kobistam.onrender.com';
 // const API_URL='http://127.0.0.1:5000';
 
 // Fetch all users
